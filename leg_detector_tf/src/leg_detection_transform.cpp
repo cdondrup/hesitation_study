dd1a42b85466a14d595a9068eb3bbd20b7c20dcb
cdondrup <cdondrup@lincoln.ac.uk>
2013-07-31 11:11:32 +0100

    Added angle calculation (might be faulty) Added debug outputs to observe angle calculation.

--- leg_detector_tf/src/leg_detection_transform.cpp
+++ leg_detector_tf/src/leg_detection_transform.cpp
@@ -45,26 +45,31 @@ void LegDetectorTf::messageCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
 	std::vector<float> legs;
 	for(int i = 0; i < msg->intensities.size(); i++) {
 		if(msg->intensities[i] == 1.0) {
-		    transformLaserToBase(polarToCartesian(msg->ranges[i], i)); //TODO: calculate angle.
+		    float angle = msg->angle_min + ((i+1) * msg->angle_increment);
+		    ROS_DEBUG("LegDetectorTf::messageCallback: Angel calculation: a_min:%f + ((i:%i+1) * a_inc:%f) = %f", msg->angle_min, i, msg->angle_increment, angle);
+		    transformLaserToBase(polarToCartesian(msg->ranges[i], angle)); //TODO: calculate angle.
 			legs.push_back(msg->ranges[i]);
-//			ROS_INFO("Range: %f", msg->ranges[i]);
+			ROS_DEBUG("LegDetectorTf::messageCallback: Range: %f", msg->ranges[i]);
 			distance.max = msg->ranges[i] > distance.max ? msg->ranges[i] : distance.max;
 			distance.min = msg->ranges[i] < distance.min ? msg->ranges[i] : distance.min;
 			distance.avg += msg->ranges[i];
 		}
 	}
 	distance.avg /= legs.size();
+	ROS_DEBUG("LegDetectorTf::messageCallback: Range: Min: %f, Max: %f, Avg: %f", distance.min, distance.max, distance.avg);
 	distance.header.stamp = ros::Time::now();
 	publishMessage();
 } // end publishCallback()
 
 geometry_msgs::PointStamped LegDetectorTf::polarToCartesian(float dist, float angle) {
+    ROS_DEBUG("LegDetectorTf::polarToCartesian: Received: distance: %f, angle: %f", dist, angle);
     geometry_msgs::PointStamped output;
     output.header.frame_id = "/base_laser_link";
     output.header.stamp = ros::Time();
     output.point.x = dist * cos(angle);
     output.point.y = dist * sin(angle);
     output.point.z = 0.365;
+    ROS_DEBUG("LegDetectorTf::polarToCartesian: Cartesian point: x: %f, y: %f, z %f", output.point.x, output.point.y, output.point.z);
     return output;
 }
 
@@ -73,11 +78,11 @@ geometry_msgs::PointStamped LegDetectorTf::transformLaserToBase(geometry_msgs::P
     try {
         listener.transformPoint("/base_link", input, output);
 
-        ROS_INFO("base_laser: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
+        ROS_DEBUG("LegDetectorTf::transformLaserToBase: base_laser: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
             input.point.x, input.point.y, input.point.z,
             output.point.x, output.point.y, output.point.z, output.header.stamp.toSec());
     } catch(tf::TransformException& ex) {
-        ROS_ERROR("Received an exception trying to transform a point from \"base_laser\" to \"base_link\": %s", ex.what());
+        ROS_ERROR("Received an exception trying to transform a point from \"base_laser_link\" to \"base_link\": %s", ex.what());
     }
     return output;
 }
