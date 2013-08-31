31801d59802c49883a4d9dd30019c5d6240bdf2a
cdondrup <cdondrup@lincoln.ac.uk>
2013-08-31 18:08:01 +0100

    Changed max_rot_vel. Needs to be 1.0

--- restaurant_state_machine/scripts/choose_behaviour.py
+++ restaurant_state_machine/scripts/choose_behaviour.py
@@ -30,14 +30,14 @@ class ChooseBehaviour(smach.State):
             self.gazingClient.send_goal(gaze)
             goal.time_to_reset = 5.0
             goal.max_vel_x     = 0.55
-            goal.max_rot_vel   = 0.4
+            goal.max_rot_vel   = 1.0
             goal.max_dist      = 6.0
             goal.min_dist      = 1.5
         else:
             self.gazingClient.cancel_all_goals()
             goal.time_to_reset = 2.0
             goal.max_vel_x     = 0.75
-            goal.max_rot_vel   = 0.5
+            goal.max_rot_vel   = 1.0
             goal.max_dist      = 1.0
             goal.min_dist      = 0.2
         self.behaviourClient.send_goal(goal)
