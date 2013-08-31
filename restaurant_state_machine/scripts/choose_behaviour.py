b6c3d543c40421152b7ab21bd4bbf1de45978177
cdondrup <cdondrup@lincoln.ac.uk>
2013-08-31 13:52:23 +0100

    Setting the rotation velocity

--- restaurant_state_machine/scripts/choose_behaviour.py
+++ restaurant_state_machine/scripts/choose_behaviour.py
@@ -21,14 +21,16 @@ class ChooseBehaviour(smach.State):
         goal.seconds = 300.0
         if toggle == 0:
             goal.time_to_reset = 5.0
-            goal.max_vel_x = 0.55
-            goal.max_dist = 6.0
-            goal.min_dist = 1.5
+            goal.max_vel_x     = 0.55
+            goal.max_rot_vel   = 0.4
+            goal.max_dist      = 6.0
+            goal.min_dist      = 1.5
         else:
             goal.time_to_reset = 2.0
-            goal.max_vel_x = 0.75
-            goal.max_dist = 1.0
-            goal.min_dist = 0.2
+            goal.max_vel_x     = 0.75
+            goal.max_rot_vel   = 0.5
+            goal.max_dist      = 1.0
+            goal.min_dist      = 0.2
         self.behaviourClient.send_goal(goal)
         rospy.loginfo("Choosing behaviour: %s", toggle)
         return 'behaviour_set'
